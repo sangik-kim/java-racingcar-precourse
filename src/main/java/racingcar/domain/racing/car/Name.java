278a6a6a7e0afe993f0dc625e8d3b44698dd2699
SANGIK KIM <sangik.kim@enel.com>
2022-04-23 20:09:07 +1200

    fix(Name): add non null validation for name

--- src/main/java/racingcar/domain/racing/car/Name.java
+++ src/main/java/racingcar/domain/racing/car/Name.java
@@ -1,8 +1,11 @@
 package racingcar.domain.racing.car;
 
+import java.util.Objects;
+
 public class Name {
     private static final int MAX_LENGTH = 5;
-    private static final String INVALID_NAME_ERROR = "자동차 이름은 5자 이하만 가능합니다.";
+    private static final String NULL_NAME_ERROR = "자동차 이름은 null이 될 수 없습니다.";
+    private static final String INVALID_NAME_ERROR = "자동차 이름은 " + MAX_LENGTH + "자 이하만 가능합니다.";
 
     private final String name;
 
@@ -21,8 +24,19 @@ public class Name {
     }
 
     private void validate(String value) {
+        validateNonNull(value);
+        validateMaxLength(value);
+    }
+
+    private void validateMaxLength(String value) {
         if (value.length() > MAX_LENGTH) {
             throw new IllegalArgumentException(INVALID_NAME_ERROR);
+        }
+    }
+
+    private void validateNonNull(String value) {
+        if (Objects.isNull(value)) {
+            throw new IllegalArgumentException(NULL_NAME_ERROR);
         }
     }
 }
