982e32de858e8ea62303d712723c1c1b18b1ff8b
SANGIK KIM <sangik.kim@enel.com>
2022-04-24 20:49:33 +1200

    feat(RacingCarGameView): add racing car game view
    - add RacingCarGameView class
    - add ViewConstant class to manage messages
    - add CarNames dto to transfer the string user input to Controller

---
+++ src/main/java/racingcar/dto/CarNames.java
@@ -0,0 +1,36 @@
+package racingcar.dto;
+
+import java.util.Objects;
+
+public class CarNames {
+    private static final String NULL_ERROR = "자동차 이름은 null이 될 수 없습니다.";
+    private static final String EMPTY_ERROR = "자동차 이름을 최소 1글자 이상이어야 합니다.";
+
+    private final String names;
+
+    public CarNames(String names) {
+        validate(names);
+        this.names = names;
+    }
+
+    public static CarNames from(String names) {
+        return new CarNames(names);
+    }
+
+    private void validate(String names) {
+        validateNonNull(names);
+        validateNonEmpty(names);
+    }
+
+    private void validateNonEmpty(String names) {
+        if (names.isEmpty()) {
+            throw new IllegalArgumentException(EMPTY_ERROR);
+        }
+    }
+
+    private void validateNonNull(String names) {
+        if (Objects.isNull(names)) {
+            throw new IllegalArgumentException(NULL_ERROR);
+        }
+    }
+}
---
+++ src/main/java/racingcar/view/RacingCarGameView.java
@@ -0,0 +1,28 @@
+package racingcar.view;
+
+import static racingcar.view.constant.ViewConstant.GET_CAR_NAMES;
+import static racingcar.view.constant.ViewConstant.GET_TRY_COUNT;
+import static racingcar.view.constant.ViewConstant.SHOW_RESULT;
+
+import racingcar.dto.CarNames;
+import racingcar.view.userinferface.GameUserInterface;
+
+public class RacingCarGameView {
+    private final GameUserInterface gameUserInterface;
+
+    public RacingCarGameView(GameUserInterface gameUserInterface) {
+        this.gameUserInterface = gameUserInterface;
+    }
+
+    public CarNames getCarNames() {
+        return CarNames.from(gameUserInterface.getUserInput(GET_CAR_NAMES));
+    }
+
+    public String getTryCount() {
+        return gameUserInterface.getUserInput(GET_TRY_COUNT);
+    }
+
+    public void showResult() {
+        gameUserInterface.show(SHOW_RESULT);
+    }
+}
