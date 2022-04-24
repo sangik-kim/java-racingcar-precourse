92f99e7cdb68c408cbcc9ea8097073c1eb7139e9
SANGIK KIM <sangik.kim@enel.com>
2022-04-25 01:21:37 +1200

    feat(RacingCarGameExceptionHandler): add a global exception handler

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 import racingcar.controller.RacingCarGameController;
+import racingcar.exception.RacingCarGameExceptionHandler;
 import racingcar.view.RacingCarGameView;
 import racingcar.view.userinferface.ConsoleUserInterface;
 import racingcar.view.userinferface.GameUserInterface;
@@ -10,9 +11,19 @@ import racingcar.view.userinferface.scanner.ConsoleScanner;
 public class Application {
     public static void main(String[] args) {
         GameUserInterface gameUserInterface = new ConsoleUserInterface(new ConsolePrinter(), new ConsoleScanner());
+        setGlobalExceptionHandler(gameUserInterface);
+        createRacingCarGameController(gameUserInterface).startGame();
+    }
+
+    private static RacingCarGameController createRacingCarGameController(GameUserInterface gameUserInterface) {
         RacingCarGameView gameView = new RacingCarGameView(gameUserInterface);
 
         RacingCarGameController gameController = new RacingCarGameController(gameView);
-        gameController.startGame();
+        return gameController;
+    }
+
+    private static void setGlobalExceptionHandler(GameUserInterface gameUserInterface) {
+        RacingCarGameExceptionHandler exceptionHandler = new RacingCarGameExceptionHandler(gameUserInterface);
+        Thread.setDefaultUncaughtExceptionHandler(exceptionHandler);
     }
 }
